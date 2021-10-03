1c99aba36526f8cec3624a00616bd8eccd729751
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2021-10-02 17:21:26 -0700

    added 2-D and 3-D examples to Array file

--- Arrays.cpp
+++ Arrays.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    
+    // 1-D:
     int grade1[5];
     int length1 = sizeof(grade1) / sizeof(grade1[0]);
     cout << "Number of elements in the array: " << length1 << endl;
@@ -34,4 +34,15 @@ int main()
     {
         cout << grade3[i] << endl;  // prints 0s for uninitialized elements which aren't valid values
     }
+
+    // 2-D:
+    int grade4[4][5];  // has 4 rows, each row contains 5 elements, 20 total elements
+    grade4[2][1] = 25;  // to initialize elements one at a time
+    cout << "2-D valid element example: " << grade4[2][1] << endl;  // program still works if grade4[30][50]
+    cout << "2-D invalid element example: " << grade4[30][50] << endl;
+
+    int grade5[2][3] = { {10,11,12}, {15,16,17} };  // declared and initialized example
+
+    // 3-D:
+    int grade6[4][2][2];  // declared and uninitialized example
 }
