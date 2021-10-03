5678c1170bab9eadee56ecbd3393d3f6f08ca470
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2021-10-02 23:10:09 -0700

    started Strings file

---
+++ Strings.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+//#include <cstring>
+using namespace std;
+
+int main()
+{
+	char s1[50] = "Hello world!";
+	char s2[50] = "";
+	char s3[] = { 'g','o','o','d' };
+	
+	cout << "s1 = " << s1 << endl;
+	cout << "s2 = " << s2 << endl;
+	cout << "s3 = " << s3 << endl;
+	cout << endl;
+
+	cout << "Enter in a few words:" << endl;
+	cin.get(s2, 50);  // user can input up to 50 chars
+	cout<<"You entered/s2 = " << s2 << endl;
+	cout << endl;
+
+	//strcpy(s1, s2);  // old "unsafe" version that causes error
+	strcpy_s(s1, s2);  // new "safe" version
+	cout << "Copied s2 to s1:" << endl;
+	cout << "s1=" << s1 << endl;
+    cout << "s2=" << s2 << endl;
+	cout << endl;
+
+	char s4[20] = "good";
+	char s5[] = "night";
+	cout << "s4 = " << s4 << endl;
+	cout << "s5 = " << s5 << endl;
+	strcat_s(s4, s5);  // concatenate the contents of s5 to the contents of s4
+	cout << "s4 after concatenation: " << s4 << endl;
+	cout << endl;
+
+	// comparing strings - only the result of 0 means they're an exact match
+	cout << "Results of comparing s4 and s5 (don't match): " << strcmp(s4, s5) << endl;
+	char s6[] = "hi";
+	char s7[] = "hi";
+	cout << "Results of comparing s6 and s7 (match): " << strcmp(s6, s7) << endl;
+	cout << endl;
+
+	// string length function:
+	cout << "Length of s7: " << strlen(s7) << endl;
+	cout << endl;
+
+
+}
