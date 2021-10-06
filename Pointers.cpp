17928970ec2779900f2a7c4c848f07b9b1e16784
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2021-10-05 18:30:02 -0700

    added to Pointers file

--- Pointers.cpp
+++ Pointers.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main()
 {
+	
 	int a = 12;
+	// int *aPointer = &a;
 	int *aPointer;
 	aPointer = &a;
 
@@ -12,9 +14,27 @@ int main()
 	cout << "Value of aPointer = " << aPointer << endl;
 	cout << "Address of aPointer = " << &aPointer << endl;
 	cout << "Printing *aPointer prints the value stored in that address: " << *aPointer << endl;
+	cout << endl;
+
+	/*
+	int b = 10;
+	int c = 5;
+
+	b = c + *aPointer;
+	cout << b << endl;
+
+	b = c *(*aPointer);
+	cout << b << endl;
+	*/
 
 
-
+	* aPointer = 4;
+	cout << "Ran aPointer = 4;" << endl;
+	cout << "Value of a = " << a << endl;
+	cout << "Address of a = " << &a << endl;
+	cout << "Value of aPointer = " << aPointer << endl;
+	cout << "Address of aPointer = " << &aPointer << endl;
+	cout << "Printing *aPointer prints the value stored in that address: " << *aPointer << endl;
 
 
 }
